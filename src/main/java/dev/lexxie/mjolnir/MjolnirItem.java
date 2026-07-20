827b2b2a17a3d75cea29f373044a6f1363430587
Lexxie Raven Black <lexxie.raven.black@proton.me>
2026-07-20 23:52:17 +0100

    Item is rendered in hand and in inventory

--- src/main/java/dev/lexxie/mjolnir/MjolnirItem.java
+++ src/main/java/dev/lexxie/mjolnir/MjolnirItem.java
@@ -1,16 +1,18 @@
 package dev.lexxie.mjolnir;
 
 import com.geckolib.animatable.GeoItem;
+import com.geckolib.animatable.client.GeoRenderProvider;
 import com.geckolib.animatable.instance.AnimatableInstanceCache;
 import com.geckolib.animatable.manager.AnimatableManager;
+import com.geckolib.renderer.GeoItemRenderer;
 import com.geckolib.util.GeckoLibUtil;
+import com.google.common.base.Suppliers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntitySpawnReason;
@@ -23,13 +25,30 @@ import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.component.TypedEntityData;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.jspecify.annotations.Nullable;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class MjolnirItem extends Item implements GeoItem {
 	private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
 
 	public MjolnirItem( Properties properties ) {
 		super(properties);
+	}
+
+	@Override
+	public void createGeoRenderer( Consumer<GeoRenderProvider> consumer ) {
+		consumer.accept(new GeoRenderProvider() {
+			private final Supplier<GeoItemRenderer<MjolnirItem>> renderer =
+					Suppliers.memoize(() -> new GeoItemRenderer<>(MjolnirItem.this));
+
+			@Override
+			public @Nullable GeoItemRenderer<MjolnirItem> getGeoItemRenderer() {
+				return this.renderer.get();
+			}
+		});
 	}
 
 	@Override
