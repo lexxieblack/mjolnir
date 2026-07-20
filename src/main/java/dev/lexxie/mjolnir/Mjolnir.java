81b6bfdd1b9a4b3fde4f4d85e1d56c26792231b9
Lexxie Raven Black <lexxie.raven.black@proton.me>
2026-07-20 22:52:39 +0100

    Checkpoint

--- src/main/java/dev/lexxie/mjolnir/Mjolnir.java
+++ src/main/java/dev/lexxie/mjolnir/Mjolnir.java
@@ -1,5 +1,6 @@
 package dev.lexxie.mjolnir;
 
+import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
@@ -8,29 +9,30 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.RegisterEvent;
+import org.slf4j.Logger;
 
 import java.util.function.Supplier;
 
 @Mod( Mjolnir.MOD_ID )
+@EventBusSubscriber
 public class Mjolnir {
+	public static final Logger LOGGER = LogUtils.getLogger();
 	public static final String MOD_ID = "mjolnir";
 
 	public static final DeferredRegister.Entities ENTITY_TYPES = DeferredRegister.createEntities(MOD_ID);
-	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
+	//	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
 
 	public static final Supplier<EntityType<MjolnirEntity>> MJOLNIR_ENTITY_TYPE = ENTITY_TYPES.register("mjolnir",
 			() -> EntityType.Builder.of(MjolnirEntity::new, MobCategory.MISC).noSummon().noLootTable().fireImmune().build(ResourceKey.create(Registries.ENTITY_TYPE, Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir"))));
 
-	public static final DeferredItem<Item> MJOLNIR_ITEM = ITEMS.registerItem("mjolnir", MjolnirItem::new);
+	//	public static final DeferredItem<Item> MJOLNIR_ITEM = ITEMS.registerItem("mjolnir", MjolnirItem::new);
 
 	@SubscribeEvent
 	public static void registerEntityRenderers( EntityRenderersEvent.RegisterRenderers event ) {
@@ -40,20 +42,22 @@ public class Mjolnir {
 	@SubscribeEvent
 	public static void buildContents( BuildCreativeModeTabContentsEvent event ) {
 		if( event.getTabKey() != CreativeModeTabs.COMBAT ) return;
-		event.accept(Mjolnir.MJOLNIR_ITEM.get());
+		//		event.accept(Mjolnir.MJOLNIR_ITEM.get());
+		event.accept(BuiltInRegistries.ITEM.getValue(Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir")));
 	}
 
 	@SubscribeEvent
 	public static void registerItems( RegisterEvent event ) {
-		event.register(BuiltInRegistries.ITEM, registry -> {
-//			registry.register(Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir"), MjolnirItem::new);
-//			registry.register(Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir"), MJOLNIR_ITEM.get());
-//			registry.register(Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir"), new MjolnirItem(new Item.Properties()));
-			registry.register(Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir"), new Item(new Item.Properties()));
+		event.register(BuiltInRegistries.ITEM.key(), registry -> {
+			//						registry.register(Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir"), MjolnirItem::new);
+			//						registry.register(Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir"), MJOLNIR_ITEM.get());
+			registry.register(Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir"),
+					new MjolnirItem(new Item.Properties().setId(ResourceKey.create(registry,
+							Identifier.fromNamespaceAndPath(MOD_ID, "mjolnir")))));
 		});
 
-		event.register(BuiltInRegistries.BLOCK, registry -> {
-			registry.register(Identifier.fromNamespaceAndPath(MOD_ID, "test"), new Block(BlockBehaviour.Properties.of()));
-		});
+		//		event.register(BuiltInRegistries.CREATIVE_MODE_TAB.key(),  registry -> {
+		//			registry.register();
+		//		});
 	}
 }
